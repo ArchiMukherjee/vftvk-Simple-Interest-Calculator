7134da35b70482ff2e1d046d82b578c4d51f3936
ArchiMukherjee <66166706+ArchiMukherjee@users.noreply.github.com>
2021-03-29 13:33:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,17 +12,22 @@ function calculate(){
 
          
 
-        if (p < 0){
-            alert("Please enter a positive number");
-            p.focus();
-            return false;
-       }
-        else{
-            alert("Form validation is successful.")
-            output.innerHTML="If you deposit "+"<mark>"+p+"</mark>"+",<br>"+
-        "at an interest rate of "+"<mark>"+r+"</mark>"+"%.<br>"+"You will receive an amount of "+"<mark>"+(p*t*r/100)+"</mark>"
+         if(p == ""){
+        alert("Amount should not be empty");
+        document.getElementById("p").focus();
+        return false;
+    }
+    else if(p <= 0 )
+    {
+        alert("Enter a positive Amount");
+        document.getElementById("p").focus();
+    }
+    else
+    {
+        output.innerHTML="If you deposit "+"<mark>"+p+"</mark>"+",<br>"+
+        "at an interest rate of "+"<mark>"+r+"</mark>"+"%.<br>"
+        +"You will receive an amount of "+"<mark>"+(p*t*r/100)+"</mark>"
         +",<br>"+"in the year "+"<mark>"+matureYear+"</mark>";
-            
     }
         }
        
