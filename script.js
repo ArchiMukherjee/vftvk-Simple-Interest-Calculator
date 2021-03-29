fa9581390a43da84472f80c7b08a6c3094c33b17
ArchiMukherjee <66166706+ArchiMukherjee@users.noreply.github.com>
2021-03-29 12:53:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
+function calculate(){
+
         
+        
+        p = document.getElementById("p").value;
+        t = document.getElementById("t").value;
+        r = document.getElementById("r").value;
+        var year = parseInt(new Date ().getFullYear());
+        T = parseInt(t)
+    
+        const matureYear =  year + T;
+
+         
+
+        if (p < 0){
+            alert("Please enter a positive number");
+            p.focus();
+            return false;
+       }
+        else{
+            alert("Form validation is successful.")
+            output.innerHTML="If you deposit "+"<mark>"+p+"</mark>"+",<br>"+
+        "at an interest rate of "+"<mark>"+r+"</mark>"+"%.<br>"+"You will receive an amount of "+"<mark>"+(p*t*r/100)+"</mark>"
+        +",<br>"+"in the year "+"<mark>"+matureYear+"</mark>";
+            
+    }
+        }
+       
+         
+    var slider = document.getElementById("r");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
